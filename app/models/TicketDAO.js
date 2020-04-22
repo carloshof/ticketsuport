61008dd6ecc3ea0fc3a58565acbaac32fd69f6ba
francoc <carloshof12@gmail.com>
2020-04-22 16:15:27 -0300

    Bugfix

--- app/models/TicketDAO.js
+++ app/models/TicketDAO.js
@@ -4,24 +4,23 @@ function TicketDAO(connection){
 
 }
 
+
 TicketDAO.prototype.NewTicket = function(ticket){
-	this._connection.open(function(err,mongoclient){
-		mongoclient.collection("tickets",fuction(err,collection)	{	
+	this._connection.open( function(err, mongoclient){
+		mongoclient.collection("tickets", function(err, collection){
 			collection.insert(ticket);
-			mongoclient.close();	
+
+			mongoclient.close();
 		});
 	});
 }
 
-
 TicketDAO.prototype.UpdateTicket = function(res,req){
 	this._connection.open(function(err,mongoclient){
 		mongoclient.collection("tickets",function(err,collection){
-			collection.update(
-			{_id:objectId(req.params.id)},
+			collection.update({_id:objectId(req.params.id)},
 			{$push : req.params.data},
-			{}
-				,function(err,records){
+			{},function(err,records){
 					if(err)
 					res.json(err);
 					else
